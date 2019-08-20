c9a23ed79f3d77d93a8d6c0c3cdd5d88d5d834e1
karlie0 <kamabrapa88@gmail.com>
2019-08-20 17:41:25 -0400

    Initial commit

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './components/Form.js'
+
+import './App.css';
+
+function App() {
+  return (
+    <h1>
+      <Form />
+    </h1>
+  );
+}
+
+export default App;
